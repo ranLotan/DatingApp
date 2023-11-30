b862b4084956ddd9d284320f636b96eea6865c31
ran.lotan <ran.lotan@cobwebs.com>
2023-11-30 13:27:58 +0200

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly ILogger<UsersController> _logger;
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context, ILogger<UsersController> logger)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+    [HttpGet("{id}")] // api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await _context.Users.Where(user => user.Id == id).ToArrayAsync();
+        if (user.Count() == 0)
+        {
+            return NotFound(user);
+        }
+        return Ok(user[0]);
+    }
+
+
+}
