d322c057ff875c530725f7f46c4a81bd4505cdec
ran.lotan <ran.lotan@cobwebs.com>
2023-12-25 12:33:03 +0200

    extended users table, reports table added, seed data added upon app.run if data base is empty

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using API.Extentions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Text;
 
 namespace API.Entities
 {
@@ -8,5 +10,16 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] HashedPassword { get; set; } 
         public byte[] HashedSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string city { get; set; }
+        public string country { get; set; }
+        public ICollection<AppReport> AppReports { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,7 @@
 using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.Text;
+using API.Data.SeedData;
 using API.Extentions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,12 +11,27 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
-
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope(); // get access to all services in scope
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await SeedData.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during Migration");
+
+}
 
 app.Run();
